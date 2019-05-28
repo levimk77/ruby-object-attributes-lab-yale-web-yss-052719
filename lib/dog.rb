937035a35cd0e466c9866507d4910fcb6abef9d6
Elijah Florence <efloren@siue.edu>
2019-05-28 10:31:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name 
+    @name
+  end
+  def name=(dogName)
+    @name = dogName
+  end
+  def breed 
+    @breed
+  end
+  def breed=(dogBreed)
+    @breed = dogBreed
+  end
+end
